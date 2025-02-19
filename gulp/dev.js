64fbceae7f4b4a450136d87b8c8e2b8db183516f
ponrez <anatoliyff@yandex.ru>
2025-02-20 06:44:33 +0700

    Все нужное для старта

---
+++ gulp/dev.js
@@ -0,0 +1,146 @@
+const gulp = require("gulp");
+const fileInclude = require("gulp-file-include");
+const sass = require("gulp-sass")(require("sass"));
+const sassGlob = require("gulp-sass-glob");
+const server = require("gulp-server-livereload");
+const clean = require("gulp-clean");
+const fs = require("fs");
+const plumber = require("gulp-plumber");
+const notify = require("gulp-notify");
+const webpack = require("webpack-stream");
+const babel = require("gulp-babel");
+const imagemin = require("gulp-imagemin");
+const changed = require("gulp-changed");
+const replace = require("gulp-replace");
+const imageminWebp = require("imagemin-webp");
+const rename = require("gulp-rename");
+
+gulp.task("clean:dev", function (done) {
+  if (fs.existsSync("./build/")) {
+    return gulp.src("./build/", { read: false }).pipe(clean({ force: true }));
+  }
+  done();
+});
+
+const fileIncludeSetting = {
+  prefix: "@@",
+  basepath: "@file",
+};
+
+const plumberNotify = (title) => {
+  return {
+    errorHandler: notify.onError({
+      title: title,
+      message: "Error <%= error.message %>",
+      sound: false,
+    }),
+  };
+};
+
+gulp.task("pixel-glass:dev", function () {
+  return gulp
+    .src("./src/pixel-glass/**/*") // Указываем путь к файлам Pixel Glass
+    .pipe(changed("./build/pixel-glass")) // Отслеживаем изменения
+    .pipe(gulp.dest("./build/pixel-glass")); // Копируем в папку build/pixel-glass
+});
+
+
+gulp.task("html:dev", function () {
+  return gulp
+    .src(["./src/html/**/*.html", "!./src/html/blocks/*.html"])
+    .pipe(changed("./build/", { hasChanged: changed.compareContents }))
+    .pipe(plumber(plumberNotify("HTML")))
+    .pipe(fileInclude(fileIncludeSetting))
+    .pipe(
+      replace(
+        /(?<=src=|href=|srcset=)(['"])(\.(\.)?\/)*(img|images|fonts|css|scss|sass|js|files|audio|video)(\/[^\/'"]+(\/))?([^'"]*)\1/gi,
+        "$1./$4$5$7$1"
+      )
+    )
+    .pipe(gulp.dest("./build/"));
+});
+
+gulp.task("sass:dev", function () {
+  return gulp
+    .src("./src/scss/*.scss")
+    .pipe(changed("./build/css/"))
+    .pipe(plumber(plumberNotify("SASS")))
+    .pipe(sassGlob())
+    .pipe(sass())
+    .pipe(
+      replace(
+        /(?<=src=|href=|srcset=)(['"])(\.(\.)?\/)*(img|images|fonts|css|scss|sass|js|files|audio|video)(\/[^\/'"]+(\/))?([^'"]*)\1/gi,
+        "$1./$4$5$7$1"
+      )
+    )
+    .pipe(gulp.dest("./build/css/"));
+});
+
+gulp.task("images:dev", function () {
+  return gulp
+    .src("./src/img/**/*", { encoding: false })
+    .pipe(changed("./build/img"))
+    .pipe(
+      imagemin([
+        imageminWebp({
+          quality: 85,
+        }),
+      ])
+    )
+    .pipe(rename({ extname: ".webp" }))
+    .pipe(gulp.dest("./build/img/"))
+    .pipe(gulp.src("./src/img/**/*", { encoding: false }))
+    .pipe(changed("./build/img/"))
+    .pipe(gulp.dest("./build/img/"));
+});
+
+gulp.task("fonts:dev", function () {
+  return gulp
+    .src("./src/fonts/**/*")
+    .pipe(changed("./build/fonts/"))
+    .pipe(gulp.dest("./build/fonts/"));
+});
+
+gulp.task("files:dev", function () {
+  return gulp
+    .src("./src/files/**/*")
+    .pipe(changed("./build/files/"))
+    .pipe(gulp.dest("./build/files/"));
+});
+
+gulp.task("js:dev", function () {
+  return (
+    gulp
+      .src("./src/js/*.js")
+      .pipe(changed("./build/js/"))
+      .pipe(plumber(plumberNotify("JS")))
+      // .pipe(babel())
+      .pipe(webpack(require("./../webpack.config.js")))
+      .pipe(gulp.dest("./build/js/"))
+  );
+});
+
+gulp.task("server:dev", function () {
+  return gulp.src("./build/").pipe(
+    server({
+      livereload: true,
+      open: true,
+    })
+  );
+});
+
+gulp.task("watch:dev", function () {
+  gulp.watch("./src/scss/**/*.scss", gulp.parallel("sass:dev"));
+  gulp.watch("./src/**/*.html", gulp.parallel("html:dev"));
+  gulp.watch("./src/img/**/*", gulp.parallel("images:dev"));
+  gulp.watch("./src/fonts/**/*", gulp.parallel("fonts:dev"));
+  gulp.watch("./src/files/**/*", gulp.parallel("files:dev"));
+  gulp.watch("./src/js/**/*.js", gulp.parallel("js:dev"));
+  gulp.watch("./src/pixel-glass/**/*", gulp.parallel("pixel-glass:dev")); // Добавляем для pixel-glass
+});
+
+// gulp.task("images", function () {
+//   return gulp
+//     .src("./src/img/**/*", { encoding: false })
+//     .pipe(gulp.dest("./build/img/"));
+// });
---
+++ gulp/docs.js
@@ -0,0 +1,161 @@
+const gulp = require("gulp");
+
+// html
+const fileInclude = require("gulp-file-include");
+// const htmlclean = require("gulp-htmlclean");
+const webpHTML = require("gulp-webp-html");
+
+// sass
+const sass = require("gulp-sass")(require("sass"));
+const sassGlob = require("gulp-sass-glob");
+const autoprefixer = require("gulp-autoprefixer");
+// const csso = require("gulp-csso");
+const webpCss = require("gulp-webp-css");
+
+const server = require("gulp-server-livereload");
+const clean = require("gulp-clean");
+const fs = require("fs");
+const plumber = require("gulp-plumber");
+const notify = require("gulp-notify");
+const webpack = require("webpack-stream");
+const babel = require("gulp-babel");
+
+// Img
+const imagemin = require("gulp-imagemin");
+const imageminWebp = require("imagemin-webp");
+const webp = require("gulp-webp");
+
+const changed = require("gulp-changed");
+const replace = require("gulp-replace");
+const rename = require("gulp-rename");
+
+gulp.task("clean:docs", function (done) {
+  if (fs.existsSync("./docs/")) {
+    return gulp.src("./docs/", { read: false }).pipe(clean({ force: true }));
+  }
+  done();
+});
+
+const fileIncludeSetting = {
+  prefix: "@@",
+  basepath: "@file",
+};
+
+const plumberNotify = (title) => {
+  return {
+    errorHandler: notify.onError({
+      title: title,
+      message: "Error <%= error.message %>",
+      sound: false,
+    }),
+  };
+};
+
+gulp.task("html:docs", function () {
+  return (
+    gulp
+      .src(["./src/html/**/*.html", "!./src/html/blocks/*.html"])
+      .pipe(changed("./docs/"))
+      .pipe(plumber(plumberNotify("HTML")))
+      .pipe(
+        replace(
+          /(?<=src=|href=|srcset=)(['"])(\.(\.)?\/)*(img|images|fonts|css|scss|sass|js|files|audio|video)(\/[^\/'"]+(\/))?([^'"]*)\1/gi,
+          "$1./$4$5$7$1"
+        )
+      )
+      .pipe(webpHTML())
+      // .pipe(htmlclean())
+      .pipe(gulp.dest("./docs/"))
+  );
+});
+
+// gulp.task("html:docs", function () {
+//   return gulp
+//     .src("./src/html/**/*.html")
+//     .pipe(gulp.dest("./docs/"));
+// });
+
+gulp.task("sass:docs", function () {
+  return (
+    gulp
+      .src("./src/scss/*.scss")
+      .pipe(changed("./docs/css/"))
+      .pipe(plumber(plumberNotify("SASS")))
+      .pipe(sassGlob())
+      .pipe(sass())
+      .pipe(autoprefixer())
+      .pipe(replace(/(?<=url\(['"]?)(\.\.\/)+img\//g, "../img/"))
+      // .pipe(csso())
+      .pipe(gulp.dest("./docs/css/"))
+  );
+});
+
+gulp.task("images:docs", function () {
+  return (
+    gulp
+      // .src("./src/img/**/*", { encoding: false })
+      .src(["./src/img/**/*", "!./src/img/svgicons/**/*"], { encoding: false })
+      .pipe(changed("./docs/img"))
+      .pipe(
+        imagemin([
+          imageminWebp({
+            quality: 85,
+          }),
+        ])
+      )
+      .pipe(rename({ extname: ".webp" }))
+      .pipe(gulp.dest("./docs/img/"))
+      .pipe(gulp.src("./src/img/**/*", { encoding: false }))
+      .pipe(changed("./docs/img"))
+      .pipe(
+        imagemin(
+          [
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.mozjpeg({ quality: 85, progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+          ],
+          { verbose: true }
+        )
+      )
+      .pipe(gulp.dest("./docs/img/"))
+  );
+});
+
+gulp.task("fonts:docs", function () {
+  return gulp
+    .src("./src/fonts/**/*")
+    .pipe(changed("./docs/fonts/"))
+    .pipe(gulp.dest("./docs/fonts/"));
+});
+
+// gulp.task("files:docs", function () {
+//   return gulp
+//     .src("./src/files/**/*")
+//     .pipe(changed("./docs/files/"))
+//     .pipe(gulp.dest("./docs/files/"));
+// });
+
+gulp.task("js:docs", function () {
+  return gulp
+    .src("./src/js/*.js")
+    .pipe(changed("./docs/js/"))
+    .pipe(plumber(plumberNotify("JS")))
+    .pipe(babel())
+    .pipe(webpack(require("../webpack.config.js")))
+    .pipe(gulp.dest("./docs/js/"));
+});
+
+gulp.task("server:docs", function () {
+  return gulp.src("./docs/").pipe(
+    server({
+      livereload: true,
+      open: true,
+    })
+  );
+});
+
+// gulp.task("images", function () {
+//   return gulp
+//     .src("./src/img/**/*", { encoding: false })
+//     .pipe(gulp.dest("./docs/img/"));
+// });
